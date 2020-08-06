c2dbe0e6b9594cfcd4eca3f1433db000900fac38
matthewlazewski <matthew.lazewski@gmail.com>
2020-08-06 09:05:33 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+
+  taskForm.addEventListener("submit", newTask);
 });
+
+const newTask = event => {
+  event.preventDefault();
+
+  const desc = document.getElementById("new-task-description");
+  const task = document.createElement("li")
+
+  task.innerText = desc.value;
+
+  addTask(task);
+};
+
+const addTask = task => {
+  document.getElementById("tasks").appendChild(task)
+};
